9968e80ef31264d87b94e111ac1823e63f81a957
zhiqiang.ge <zhiqiang.ge@dmall.com>
2016-05-09 14:22:04 +0800

    '修改书签存储方式'

--- chrome/bookmark.py
+++ chrome/bookmark.py
@@ -1,12 +1,28 @@
+import platform
+
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 __author__ = 'Eason'
 
+
 @csrf_exempt
 def sync(request):
     content = request.POST.get("bookmarks")
-    file_object = open('/Users/bao/Downloads/bookmarks.txt', 'w')
+
+    if content is None:
+        return HttpResponse("none")
+
+    file_path = ""
+    sysstr = platform.system()
+    if sysstr == "Windows":
+        file_path = "E:\\360CloudUI\\Cache\\30730947\\bookmarks\\bookmarks.txt"
+    elif sysstr == "Linux":
+        print("Call Linux tasks")
+    else:
+        file_path = "/Users/bao/Downloads/bookmarks.txt"
+
+    file_object = open(file_path, 'w', encoding="utf-8")
     file_object.write(content)
     file_object.close()
-    return HttpResponse("aa")
+    return HttpResponse("success")
